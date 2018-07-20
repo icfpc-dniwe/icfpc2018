9ecc58fcfa63ff21489bc5a39caf1c6a0c103558
vuvko <notvuvko@gmail.com>
2018-07-20 20:24:05 +0300

    Tensor3 data type

--- src/ICFPC2018/Tensor3.hs
+++ src/ICFPC2018/Tensor3.hs
@@ -1,8 +1,35 @@
-module ICFPC2018.Tensor3 where
+module ICFPC2018.Tensor3
+  ( Tensor3Size
+  , Tensor3Idx
+  , Tensor3
+  , index
+  , update
+  , generate
+  ) where
 
 import Data.Vector (Vector)
 import qualified Data.Vector as V
-import Data.Linear.V3 (V3)
-import qualified Data.Linear.V3 as V3
+import Linear.V3 (V3)
+import qualified Linear.V3 as V3
 
-data Tensor3 a = Tensor3 (Vector a) V3
+type Tensor3Size = V3 Int
+type Tensor3Idx = V3 Int
+
+data Tensor3 a = Tensor3 (Vector a) Tensor3Size
+
+resultIdx :: Tensor3Size -> Tensor3Idx -> Int
+resultIdx (V3.V3 xSize ySize zSize) (V3.V3 xIdx yIdx zIdx)
+  | xIdx < xSize && yIdx < ySize && zIdx < zSize && xIdx >= 0 && yIdx >= 0  && zIdx >= 0 = xIdx + yIdx * xSize + zIdx * xSize * ySize
+  | otherwise = error "Invalid index"
+
+index :: Tensor3 a -> Tensor3Idx -> a
+index (Tensor3 v size) idx = v `V.unsafeIndex` resultIdx size idx
+
+update :: Tensor3 a -> [(Tensor3Idx, a)] -> Tensor3 a
+update tensor [] = tensor
+update (Tensor3 v size) updates = Tensor3 (V.unsafeUpd v $ map (\(idx, val) -> (resultIdx size idx, val)) updates) size
+
+generate :: Vector a -> Tensor3Size -> Tensor3 a
+generate v size@(V3.V3 xSize ySize zSize)
+  | V.length v == xSize * ySize * zSize = Tensor3 v size
+  | otherwise = error "invalid tensor size"
