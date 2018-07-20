e8f8d27239fc234e363126d07b71448f1d494b9e
Nikolay Amiantov <ab@fmap.me>
2018-07-20 19:33:01 +0300

    Expose types

--- default.nix
+++ default.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, stdenv }:
+{ mkDerivation, base, binary, bytestring, linear, stdenv }:
 mkDerivation {
   pname = "icfpc2018";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
-  libraryHaskellDepends = [ base ];
+  libraryHaskellDepends = [ base binary bytestring linear ];
   executableHaskellDepends = [ base ];
   license = stdenv.lib.licenses.bsd3;
 }
