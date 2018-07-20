e8e97d18d28e8c1a31d9326f3567dbea54e35998
vuvko <notvuvko@gmail.com>
2018-07-20 21:09:14 +0300

    Types for stupid solver

--- src/ICFPC2018/Types.hs
+++ src/ICFPC2018/Types.hs
@@ -2,5 +2,13 @@ module ICFPC2018.Types where
 
 import Data.Vector (Vector)
 import ICFPC2018.Tensor3 (Tensor3)
+import Linear.V3 (V3(..))
+import qualified Linear.V3 as V3
 
 type Model = Tensor3 Bool
+
+type BotIdx = Int
+type Distance = V3 Int
+data Command = Halt | Wait | Flip | SMove Distance | LMove Distance | Fission Distance Int | Fill Int
+                              deriving (Show, Eq)
+data Trace = [(BotIdx, Command)]
