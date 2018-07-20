ff9f3ac16390d3c1249b9f32203bed77465a344c
vuvko <notvuvko@gmail.com>
2018-07-21 00:59:27 +0300

    snakeIdx now in Utils.hs

--- src/ICFPC2018/Utils.hs
+++ src/ICFPC2018/Utils.hs
@@ -1,6 +1,8 @@
 module ICFPC2018.Utils where
 
 import ICFPC2018.Types
+import ICFPC2018.Tensor3 (Tensor3Size, Tensor3Idx)
+import qualified ICFPC2018.Tensor3 as T3
 import Linear.V3 (V3(..))
 
 div1 :: Integral a => a -> a -> a
@@ -11,6 +13,32 @@ a `div1` b
 
 infixl 7  `div1`
 
+{-
+moving across Tensor3 starting from (0, 0, 0) along X first then Z and finally Y.
+Y == 0 is bottom plane.
+-}
+snakeIdx :: Tensor3Size -> [Tensor3Idx]
+snakeIdx (V3 xSize ySize zSize) = helpFold (V3 0 0 0) False
+  where
+    helpFold (V3 xIdx yIdx zIdx) ySwitch
+      | yIdx >= ySize = []
+      | firstX = map xToV3 [0 .. xSize - 1] ++ changeZ (xSize - 1)
+      | lastX = map xToV3 [xSize - 1, xSize - 2 .. 0] ++ changeZ 0
+      | otherwise = undefined
+      where
+        firstX = xIdx == 0
+        firstY = yIdx == 0
+        firstZ = zIdx == 0
+        lastX = xIdx == xSize - 1
+        lastY = yIdx == ySize - 1
+        lastZ = zIdx == zSize - 1
+        xToV3 x = (V3 x yIdx zIdx)
+        changeZ x | lastZ && not ySwitch = changeY x
+                  | firstZ && ySwitch    = changeY x
+                  | not ySwitch = helpFold (V3 x yIdx (zIdx + 1)) ySwitch
+                  | ySwitch = helpFold (V3 x yIdx (zIdx - 1)) ySwitch
+        changeY x = helpFold (V3 x (yIdx + 1) zIdx) (not ySwitch)
+
 
 mkLinearDifference :: Axis -> Int -> V3 Int
 mkLinearDifference X v = V3 v 0 0
@@ -18,7 +46,7 @@ mkLinearDifference Y v = V3 0 v 0
 mkLinearDifference Z v = V3 0 0 v
 
 
-packMove :: VolatileCoordinate -> VolatileCoordinate -> [Command] 
+packMove :: VolatileCoordinate -> VolatileCoordinate -> [Command]
 packMove = error "TODO"
 
 simulateStep :: VolatileCoordinate -> Command -> [VolatileCoordinate]
