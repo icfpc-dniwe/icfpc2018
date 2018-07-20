0b6b0f5799e25286aea9a40f02b26f66fc5b839c
Nikolay Amiantov <ab@fmap.me>
2018-07-20 20:30:42 +0300

    Add IO

---
+++ src/ICFPC2018/IO.hs
@@ -0,0 +1,29 @@
+module ICFPC2018.IO
+  ( getModel
+  ) where
+
+import Data.Word
+import Data.Bits
+import Linear.V3 (V3(..))
+import qualified Data.Vector as V
+import Data.Binary.Get
+
+import ICFPC2018.Types
+import qualified ICFPC2018.Tensor3 as T3
+
+getBits :: Int -> Get [Bool]
+getBits size = go size 0 0 []
+  where go :: Int -> Int -> Word8 -> [Bool] -> Get [Bool]
+        go 0 _ _ res = return $ reverse res
+        go left 0 _ res = do
+          newByte <- getWord8
+          go left 8 newByte res
+        go left byteLeft byte res = go (left - 1) (byteLeft - 1) (byte `shiftL` 1) (((byte .&. 1) == 1) : res)
+
+getModel :: Get Model
+getModel = do
+  resolution <- fromIntegral <$> getWord8
+  let size = resolution * resolution * resolution
+  bitsData <- getBits size
+  let vec = V.fromList bitsData
+  return $ T3.generate vec (V3 resolution resolution resolution)
--- src/ICFPC2018/Types.hs
+++ src/ICFPC2018/Types.hs
@@ -1,1 +1,6 @@
 module ICFPC2018.Types where
+
+import Data.Vector (Vector)
+import ICFPC2018.Tensor3 (Tensor3)
+
+type Model = Tensor3 Bool
