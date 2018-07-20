16b26d1371992695d0907096fa7ae8ba44653906
vuvko <notvuvko@gmail.com>
2018-07-20 20:35:28 +0300

    fix generator V3

--- src/ICFPC2018/Tensor3.hs
+++ src/ICFPC2018/Tensor3.hs
@@ -9,7 +9,7 @@ module ICFPC2018.Tensor3
 
 import Data.Vector (Vector)
 import qualified Data.Vector as V
-import Linear.V3 (V3)
+import Linear.V3 (V3(..))
 import qualified Linear.V3 as V3
 
 type Tensor3Size = V3 Int
@@ -18,8 +18,11 @@ type Tensor3Idx = V3 Int
 data Tensor3 a = Tensor3 (Vector a) Tensor3Size
 
 resultIdx :: Tensor3Size -> Tensor3Idx -> Int
-resultIdx (V3.V3 xSize ySize zSize) (V3.V3 xIdx yIdx zIdx)
-  | xIdx < xSize && yIdx < ySize && zIdx < zSize && xIdx >= 0 && yIdx >= 0  && zIdx >= 0 = xIdx + yIdx * xSize + zIdx * xSize * ySize
+resultIdx (V3 xSize ySize zSize) (V3 xIdx yIdx zIdx)
+  | xIdx < xSize && xIdx >= 0 &&
+    yIdx < ySize && yIdx >= 0 &&
+    zIdx < zSize && zIdx >= 0
+      = xIdx + yIdx * xSize + zIdx * xSize * ySize
   | otherwise = error "Invalid index"
 
 index :: Tensor3 a -> Tensor3Idx -> a
@@ -30,6 +33,6 @@ update tensor [] = tensor
 update (Tensor3 v size) updates = Tensor3 (V.unsafeUpd v $ map (\(idx, val) -> (resultIdx size idx, val)) updates) size
 
 generate :: Vector a -> Tensor3Size -> Tensor3 a
-generate v size@(V3.V3 xSize ySize zSize)
+generate v size@(V3 xSize ySize zSize)
   | V.length v == xSize * ySize * zSize = Tensor3 v size
   | otherwise = error "invalid tensor size"
