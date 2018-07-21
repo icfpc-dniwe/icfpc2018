14ec56c92d906538f3368f8635478e5344d32425
Nikolay Amiantov <ab@fmap.me>
2018-07-21 04:24:07 +0300

    Fix score a bit

--- exe/Solver.hs
+++ exe/Solver.hs
@@ -9,6 +9,7 @@ import ICFPC2018.IO
 import ICFPC2018.Simulation
 import ICFPC2018.Validation
 import ICFPC2018.Solvers.HighSolver
+import ICFPC2018.Scoring
 
 main :: IO ()
 main = do
@@ -16,7 +17,9 @@ main = do
   modelData <- BL.readFile modelPath
   let model = runGet getModel modelData
       trace = packIntensions (solver model) (SingleBotModel (V3 0 0 0) model)
+      score = scoreTrace model trace
       traceData = runPut $ putTrace trace
   print trace
+  print score
   unless (validTrace trace) $ fail "Invalid trace"
   BL.writeFile tracePath traceData
--- src/ICFPC2018/Scoring.hs
+++ src/ICFPC2018/Scoring.hs
@@ -4,12 +4,13 @@ module ICFPC2018.Scoring
   , scoreTrace
   ) where
 
-import ICFPC2018.Types
-import ICFPC2018.Tensor3
-import ICFPC2018.Utils
+import qualified Data.Vector as V
+import Linear.V3 (V3(..))
 import Debug.Trace
 
-import qualified Data.Vector as V
+import ICFPC2018.Types
+import qualified ICFPC2018.Tensor3 as T3
+import ICFPC2018.Utils
 
 scoreCommand :: Model -> Command -> Score
 scoreCommand _ Halt = 0
@@ -18,7 +19,7 @@ scoreCommand _ Flip = 0
 scoreCommand _ (SMove lld) = 2 * mlen lld
 scoreCommand _ (LMove sld1 sld2) = 2 * (clen sld1 + clen sld2 + 2)
 scoreCommand _ (Fission _ _) = 24
-scoreCommand m (Fill nd) = if m ! nd then 6 else 12
+scoreCommand m (Fill nd) = if m T3.! nd then 6 else 12
 scoreCommand _ (FusionP _) = -24
 scoreCommand _ (FusionS _) = 0
 
@@ -35,9 +36,10 @@ scoreStep m s = (score, stepFlipsHarmonic s) where
     score = localCost + (sum $ (scoreCommand m) <$> s)
     localCost = 20 * length s
 
-scoreTrace' :: HarmonicState -> Model -> Int {-R-} -> Trace -> Score
-scoreTrace' _ _ _ [] = 0
-scoreTrace' harmonicState model r (h:t) = trace ("h: " ++ show h ++ " harm: " ++ show newHarmonicState ++ " gs: " ++ show globalCost ++ " ts: " ++ show totalStepScore) $ globalCost + totalStepScore + scoreTrace' newHarmonicState model r t where
+scoreTrace' :: HarmonicState -> Model -> Trace -> Score
+scoreTrace' _ _ [] = 0
+scoreTrace' harmonicState model (h:t) = trace ("h: " ++ show h ++ " harm: " ++ show newHarmonicState ++ " gs: " ++ show globalCost ++ " ts: " ++ show totalStepScore) $ globalCost + totalStepScore + scoreTrace' newHarmonicState model t where
+    V3 r _ _ = T3.size model
     stepScore = scoreStep model h
     totalStepScore = fst stepScore
     flipHarmonic = snd stepScore
@@ -47,5 +49,8 @@ scoreTrace' harmonicState model r (h:t) = trace ("h: " ++ show h ++ " harm: " ++
       High -> 30
       _    -> 3
 
-scoreTrace :: Model -> Int {-R-} -> Trace -> Score
-scoreTrace model r t = scoreTrace' Low model r t
+scoreTrace :: Model -> Trace -> Score
+scoreTrace model t
+  | xS == yS && yS == zS = scoreTrace' Low model t
+  | otherwise = error "scoreTrace: invalid model"
+  where V3 xS yS zS = T3.size model
