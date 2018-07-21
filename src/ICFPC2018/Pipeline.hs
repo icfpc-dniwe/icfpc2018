4bdd16bf75191dbd83e507661e12b7daaf1d8505
vuvko <notvuvko@gmail.com>
2018-07-21 17:22:45 +0300

    uuuuaaaaaa

---
+++ src/ICFPC2018/Pipeline.hs
@@ -0,0 +1,22 @@
+module ICFPC2018.Pipeline where
+
+import ICFPC2018.Types
+import ICFPC2018.Simulations
+import ICFPC2018.Tensor3 as T3
+
+pipeline :: Model -> Trace
+pipeline = undefined
+
+spawnBots :: Int -> Trace
+spawnBots = undefined
+
+sliceModel :: Model -> Int -> [T3.BoundingBox]
+sliceModel m0 numBots =
+  where
+    modelBox = T3.boundingBox m0 id
+    bottomLine = case modelBox of
+      (V3 x1 y1 z1, V3 x2 y2 z2) -> if (x2 - x1) > (z2 - z1)
+                                    then (X, x2 - x1)
+                                    else (Z, z2 - z1)
+    slice (X, xdiff) = undefined
+    slice (Z, zdiff) = undefined
