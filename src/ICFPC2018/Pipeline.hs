31346821ab3532d01fed002b2227078a279d5fb3
vuvko <notvuvko@gmail.com>
2018-07-23 09:06:41 +0300

    Moved splitModel to separate file

--- src/ICFPC2018/Pipeline.hs
+++ src/ICFPC2018/Pipeline.hs
@@ -1,9 +1,6 @@
 module ICFPC2018.Pipeline
   ( pipeline
   , moveToZero
-  , sliceModel
-  , splitModel
-  , bboxHeuristics
   ) where
 
 import Data.Maybe
@@ -146,46 +143,3 @@ getNextLine ((FillIdx firstIdx):xs) = helper Any firstIdx xs
       | otherwise = Just ((firstIdx, lastIdx), intensions)
     helper _ lastIdx intensions = Just ((firstIdx, lastIdx), intensions)
 getNextLine _ = undefined
-
-sliceModel :: Model -> T3.Axis -> [Model]
-sliceModel model axis = map (\(begin, end) -> T3.sliceAxis model axis begin (end - 1)) boundings
-  where
-    (V3 xsz ysz zsz) = T3.size model
-    axisSize T3.X = xsz
-    axisSize T3.Y = ysz
-    axisSize T3.Z = zsz
-    sz = axisSize axis
-    indices = [0, maxFD .. (sz - 2)] ++ [sz]
-    boundings = zip (init indices) (tail indices)
-
-splitModel :: Model -> T3.Axis -> (Model, Model)
-splitModel model axis = (subModel (0, splitIdx - 1), subModel (splitIdx, sz))
-  where
-    (V3 xsz ysz zsz) = T3.size model
-    axisSize T3.X = xsz
-    axisSize T3.Y = ysz
-    axisSize T3.Z = zsz
-    sz = axisSize axis
-    step = 5
-    indices = [2, step .. (sz - 2)]
-    subModel (begin, end) = T3.sliceAxis model axis begin (end - 1)
-    heuristicsFirst = map (bboxHeuristics . subModel) $ map ((,) 0) indices
-    heuristicsSecond = map (bboxHeuristics . subModel) $ map (flip (,) sz) indices
-    heuristics = zipWith (\x y -> abs $ x - y) heuristicsFirst heuristicsSecond
-    argmax elems = snd $ foldr1 findMax $ zip elems [1..]
-      where
-        findMax f@(val, _) m@(maxVal, _) | maxVal < val = f
-                                                | otherwise = m
-    splitIdx = indices !! (argmax heuristics)
-
-bboxHeuristics :: Model -> Double
-bboxHeuristics model = sm / sz
-  where
-    bbox = T3.boundingBox model id
-    subModel = T3.slice model bbox
-    modelSum :: Int
-    modelSum = T3.nonzero subModel
-    sm :: Double
-    sm = fromIntegral modelSum
-    sz :: Double
-    sz = fromIntegral $ product $ T3.size subModel
